6e803e04f97069c65f272f97ab31f2873cf6ba1f
高峰 <gaofeng2134@outlook.com>
2025-04-02 17:08:47 +0800

    导航菜单改为setup script形式。

--- frontend/src/components/NavigateMenuComponent.vue
+++ frontend/src/components/NavigateMenuComponent.vue
@@ -11,31 +11,26 @@
   </q-item>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'NavigateMenuComponent',
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-
-    caption: {
-      type: String,
-      default: '',
-    },
-
-    link: {
-      type: String,
-      default: '#',
-    },
-
-    icon: {
-      type: String,
-      default: '',
-    },
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
   },
+
+  caption: {
+    type: String,
+    default: ''
+  },
+
+  link: {
+    type: String,
+    default: '#'
+  },
+
+  icon: {
+    type: String,
+    default: ''
+  }
 });
 </script>
